36104f185db26836b9112af5515ae81a54707fdb
Shubham Bhalerao <92851077+shubhamprogrammer2371@users.noreply.github.com>
2021-11-09 23:32:47 +0530

    Update nav.php

--- docs/nav.php
+++ docs/nav.php
@@ -11,7 +11,6 @@
   <link rel="stylesheet" type="text/css" href="bootstrap-4.5.3-dist/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="css/index.css">
   <link rel="stylesheet" type="text/css" href="css/table.css">
-  <link rel="stylesheet" type="text/css" href="css/createuser.css">
   <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css"/>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
